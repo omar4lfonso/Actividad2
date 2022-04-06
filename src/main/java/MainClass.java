3daed8038eb98271538d92839c255b23b598a1b2
Omar Solis R <omsolis@estudiantec.cr>
2022-04-05 23:16:37 -0600

    Se agregaron los outputs de bubble e insertion sort

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -12,6 +12,10 @@ public class MainClass {
         int n = arrEnteros.length;
         int temp = 0;
 
+        // TEST CODE START
+        String sEnteros = "";
+        int numIteracion = 0;
+        // TEST CODE END
         for (out = n - 1; out > 1; out--) {
             for (in = 0; in < out; in++) {
                 if (arrEnteros[in] > arrEnteros[in + 1]) {
@@ -20,6 +24,14 @@ public class MainClass {
                     arrEnteros[in] = arrEnteros[in + 1];
                     arrEnteros[in + 1] = temp;
                 }
+                // TEST CODE START
+                numIteracion++;
+                sEnteros = "";
+                for(int i = 0; i <= n-1;i++){
+                    sEnteros += Integer.toString(arrEnteros[i]) + " ";
+                }
+                System.out.println("Iteración #" + numIteracion + ": [" + sEnteros + "]");
+                // TEST CODE END
             }
         }
     }
@@ -32,14 +44,29 @@ public class MainClass {
         int in;
         int out;
         int n = arrEnteros.length;
+
+        // TEST CODE START
+        String sEnteros = "";
+        int numIteracion = 0;
+        // TEST CODE END
+
         for (out = 1; out < n; out++) {
             int key = arrEnteros[out];
-            in = out;
-            while ((in > 0) && (arrEnteros[in - 1] >= key)) {
-                arrEnteros[in] = arrEnteros[in-1];
-                --in;
+            in = out - 1;
+            while ((in > -1) && (arrEnteros[in] > key)) {
+                arrEnteros[in + 1] = arrEnteros[in];
+                in--;
+
+                // TEST CODE START
+                numIteracion++;
+                sEnteros = "";
+                for(int i = 0; i <= n-1;i++){
+                    sEnteros += Integer.toString(arrEnteros[i]) + " ";
+                }
+                System.out.println("Iteración #" + numIteracion + ": [" + sEnteros + "]");
+                // TEST CODE END
             }
-            arrEnteros[in] = key;
+            arrEnteros[in + 1] = key;
         }
     }
 
@@ -49,6 +76,7 @@ public class MainClass {
      */
     static void selectSort(int arrEnteros[]) {
         int endIndex = arrEnteros.length - 1;
+
         for (int current = 0; current < endIndex; current++) {
             swap(current, minIndex(current, endIndex, arrEnteros));
         }
@@ -63,10 +91,27 @@ public class MainClass {
 
     static int minIndex(int start, int end, int values[]) {
         int indexOfMin = start;
+
+        // TEST CODE START
+        String sEnteros = "";
+        int numIteracion = 0;
+        // TEST CODE END
+
         for (int index = start + 1; index <= end; index++){
             if (values[index] < values[indexOfMin]){
                 indexOfMin = index;
             }
+
+            // TEST CODE START
+            int n = values.length;
+            numIteracion++;
+            sEnteros = "";
+            for(int i = 0; i <= n-1;i++){
+                sEnteros += Integer.toString(values[i]) + " ";
+            }
+            System.out.println("Iteración #" + numIteracion + ": [" + sEnteros + "]");
+            // TEST CODE END
+
         }
         return indexOfMin;
     }
@@ -75,24 +120,60 @@ public class MainClass {
         int[] arrNumber = {13, 17, 12, 8, 13, 3, 7, 11, 2, 10};
         int[] arrOrdered = arrNumber.clone();
 
+        System.out.println("/*********Bubble_Sort:START********/");
         long start = System.currentTimeMillis();
         bubbleSort(arrOrdered);
         long finish = System.currentTimeMillis();
         long timeElapsed = finish - start;
         System.out.println("Bubble Sort elapsed time: " + timeElapsed);
 
-        arrOrdered = arrNumber.clone();
-        start = System.currentTimeMillis();
-        insertionSort(arrNumber);
-        finish = System.currentTimeMillis();
-        timeElapsed = finish - start;
-        System.out.println("Bubble Sort elapsed time: " + timeElapsed);
+        // TEST CODE START
+        int n = arrOrdered.length;
+        String sEnteros = "";
+        for(int i = 0; i <= n-1;i++){
+            sEnteros += Integer.toString(arrOrdered[i]) + " ";
+        }
+        System.out.println("Final output: [" + sEnteros + "]");
+        // TEST CODE END
 
+        System.out.println("/*********Bubble_Sort:End********/");
+
+
+        System.out.println("/*********Insertion_Sort:START********/");
         arrOrdered = arrNumber.clone();
         start = System.currentTimeMillis();
-        selectSort(arrNumber);
+        insertionSort(arrOrdered);
         finish = System.currentTimeMillis();
         timeElapsed = finish - start;
-        System.out.println("Bubble Sort elapsed time: " + timeElapsed);
+        System.out.println("Insertion Sort elapsed time: " + timeElapsed);
+
+        // TEST CODE START
+        n = arrOrdered.length;
+        sEnteros = "";
+        for(int i = 0; i <= n-1;i++){
+            sEnteros += Integer.toString(arrOrdered[i]) + " ";
+        }
+        System.out.println("Final output: [" + sEnteros + "]");
+        // TEST CODE END
+
+        System.out.println("/*********Insertion_Sort:END********/");
+
+        System.out.println("/*********Selection_Sort:START********/");
+        arrOrdered = arrNumber.clone();
+        start = System.currentTimeMillis();
+        selectSort(arrOrdered);
+        finish = System.currentTimeMillis();
+        timeElapsed = finish - start;
+        System.out.println("Selection Sort elapsed time: " + timeElapsed);
+
+        // TEST CODE START
+        n = arrOrdered.length;
+        sEnteros = "";
+        for(int i = 0; i <= n-1;i++){
+            sEnteros += Integer.toString(arrOrdered[i]) + " ";
+        }
+        System.out.println("Final output: [" + sEnteros + "]");
+        // TEST CODE END
+        System.out.println("/*********Selection_Sort:END********/");
     }
 }
